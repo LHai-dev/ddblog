a0a0e9cef440bb6c288b3225988e6daf0515c47d
Hai <limhaifc@gmail.com>
2024-09-07 16:37:20 +0700

    fix not found page-4

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -5,19 +5,23 @@ import { Post } from '@/app/type/type';
 import BlogDetail from '@/app/components/BlogDetail';
 
 export default async function BlogPost({ params }: { params: { slug: string } }) {
-  // Move the data fetching logic inside the component
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/blogs/${params.slug}`);
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/blogs/${params.slug}`);
 
-  if (!res.ok) {
-    notFound(); // Show 404 if not found
-  }
+    if (!res.ok) {
+      console.error('Failed to fetch post:', res.status, res.statusText); // Log error
+      notFound();
+    }
 
-  const post: Post | null = await res.json();
+    const post: Post | null = await res.json();
 
-  // If post is null, trigger not found
-  if (!post) {
+    if (!post) {
+      notFound();
+    }
+
+    return <BlogDetail post={post} />;
+  } catch (error) {
+    console.error('Error fetching post:', error); // Log the error
     notFound();
   }
-
-  return <BlogDetail post={post} />;
 }
--- app/components/Banners.tsx
+++ app/components/Banners.tsx
@@ -3,7 +3,7 @@ const Banner = () => {
     <div className="bg-indigo-600">
       <div className="max-w-screen-xl mx-auto px-4 py-3 text-white sm:text-center md:px-8">
         <p className="font-medium">
-          យើងទើបតែបើកដំណើរការប្លក់ថ្មីនៅថ្ងៃនេះ <a href="javascript:(0)" className="font-semibold underline duration-150 hover:text-indigo-100 inline-flex items-center gap-x-1">
+          យើងទើបតែបើកដំណើរការប្លក់ថ្មីនៅថ្ងៃនេះ <a href="/" className="font-semibold underline duration-150 hover:text-indigo-100 inline-flex items-center gap-x-1">
           ស្វែងយល់បន្ថែម
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
             <path fillRule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clipRule="evenodd" />
