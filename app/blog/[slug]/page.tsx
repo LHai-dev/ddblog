aebdb00d675474532688c441f7dcbd4c33daaed1
Hai <limhaifc@gmail.com>
2024-09-07 15:59:52 +0700

    fix not found page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,10 +2,17 @@ import { notFound } from 'next/navigation';
 import { Post } from '@/app/type/type';
 import BlogDetail from '@/app/components/BlogDetail';
 
-// Fetch the blog post data server-side
+export interface Params {
+  params: {
+    slug: string;
+  };
+}
+
 async function getPost(slug: string): Promise<Post | null> {
   try {
     const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || process.env.VERCEL_URL || 'http://localhost:3000';
+    console.log('Base URL in production:', baseUrl); // Log the base URL in production
+
     const res = await fetch(`${baseUrl}/api/blogs/${slug}`, { cache: 'no-store' });
 
     if (!res.ok) {
@@ -20,14 +27,12 @@ async function getPost(slug: string): Promise<Post | null> {
   }
 }
 
-export default async function BlogPost({ params }: { params: { slug: string } }) {
+export default async function BlogPost({ params }: Params) {
   const post: Post | null = await getPost(params.slug);
 
-  // If post is not found, show 404 page
   if (!post) {
-    notFound();
+    notFound(); // Trigger a 404 if post is not found
   }
 
-  // Render the BlogDetail component with the post data
   return <BlogDetail post={post} />;
 }
