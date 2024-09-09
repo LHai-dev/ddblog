e2a92fb0c7f8a1ac66267eefa029b070ef261375
Hai <limhaifc@gmail.com>
2024-09-09 17:51:35 +0700

    update page seo

---
+++ app/lib/siteMetaData.tsx
@@ -0,0 +1,19 @@
+const siteMetadata = {
+  title: 'ddkh-Blog',
+  author: 'Lim-Hai',
+  headerTitle: 'Next.js Blog',
+  description: 'A blog created with Next.js, Tailwind.css and content-layer.',
+  language: 'en-us',
+  theme: 'light', // system, dark or light
+  siteUrl: 'https://ddkhdev.lol',
+  siteLogo: '/logo.png',
+  socialBanner: '/social-banner.png',
+  email: 'limhaifc@gmail.com',
+  github: 'https://github.com/LHai-dev',
+  twitter: 'https://twitter.com/hai172',
+  facebook: 'https://www.facebook.com/ahhai12345',
+  linkedin: 'https://www.linkedin.com/in/lun-lim-hai-9141a82a0',
+  locale: 'en-US',
+};
+
+export default siteMetadata; // Use ES module export
