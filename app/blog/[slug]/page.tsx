b632a9aaff11ad5d0a97a3dfba2bb79f7188dd9a
Hai <limhaifc@gmail.com>
2024-09-09 19:00:03 +0700

    add seo to slug blog detail

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,9 +1,9 @@
-// app/blog/[slug]/page.tsx (Server Component)
 import { notFound } from 'next/navigation';
 import { Post } from '@/app/type/Post';
 import BlogDetail from '@/app/components/BlogDetail';
 import { serialize } from 'next-mdx-remote/serialize';
 import { MDXRemoteSerializeResult } from 'next-mdx-remote';
+import Head from 'next/head';
 
 export default async function BlogPost({ params }: { params: { slug: string } }) {
   try {
@@ -26,7 +26,26 @@ export default async function BlogPost({ params }: { params: { slug: string } })
     const mdxSource: MDXRemoteSerializeResult = await serialize(post.content);
 
     // Render the BlogDetail component, passing the post and serialized MDX content
-    return <BlogDetail post={post} mdxSource={mdxSource} />;
+    return (
+      <>
+        {/* Add dynamic SEO for the blog post */}
+        <Head>
+          <title>{post.title} | My Blog</title>
+          <meta name="description" content={post.summary} />
+          <meta property="og:title" content={post.title} />
+          <meta property="og:description" content={post.summary} />
+          <meta property="og:image" content={post.thumbnailUrl} />
+          <meta property="og:url" content={`https://ddkhdev.lol/api/blogs/${post.slug}`} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content={post.title} />
+          <meta name="twitter:description" content={post.summary} />
+          <meta name="twitter:image" content={post.authorImageUrl} />
+        </Head>
+
+        {/* Render the blog detail */}
+        <BlogDetail post={post} mdxSource={mdxSource} />
+      </>
+    );
   } catch (error) {
     // Log any errors that occur during the fetch process
     console.error('Error fetching post:', error);
