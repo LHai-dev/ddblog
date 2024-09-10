ad89a9a75e1c5c1de8716879c119fb9d210cf3f6
Hai <limhaifc@gmail.com>
2024-09-10 16:27:33 +0700

    test seo page detail 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/app/components/Navbar";
 import React from "react";
 import { Noto_Sans_Khmer } from 'next/font/google';
 import Banner from "@/app/components/Banners";
+import siteMetaData from "@/app/lib/siteMetaData";
 export const runtime = 'edge';
 // Load the custom font
 const notoSansKhmer = Noto_Sans_Khmer({
@@ -11,7 +12,52 @@ const notoSansKhmer = Noto_Sans_Khmer({
   variable: '--font-notoSansKhmer',
 });
 
-
+// Define the metadata for SEO and social sharing
+export const metadata = {
+  metadataBase: new URL(siteMetaData.siteUrl),
+  title: {
+    template: `%s | ${siteMetaData.title}`, // Dynamic title
+    default: siteMetaData.title, // Default title when no template
+  },
+  description: siteMetaData.description, // Default description
+  openGraph: {
+    title: siteMetaData.title,
+    description: siteMetaData.description,
+    url: siteMetaData.siteUrl,
+    siteName: siteMetaData.title,
+    images: [
+      {
+        url: siteMetaData.socialBanner,
+        width: 1200, // Specify image dimensions
+        height: 630,
+        alt: "Banner image for social sharing", // Provide an alt for accessibility
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteMetaData.title,
+    description: siteMetaData.description,
+    images: [siteMetaData.socialBanner],
+  },
+  robots: {
+    index: true, // Ensure search engines index the page
+    follow: true, // Allow following links
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true, // Prevent image indexing
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  alternates: {
+    canonical: siteMetaData.siteUrl, // Canonical URL for SEO
+  },
+};
 // Root layout function wrapping children components
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
