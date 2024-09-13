29c6eeb87e761e7426691a8bc6bfcf0c801d0739
Hai <limhaifc@gmail.com>
2024-09-13 22:41:43 +0700

    chnage google to git hub

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,11 @@
 import NextAuth from "next-auth";
-import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
 
 export const { auth, handlers: { GET, POST}} = NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string
     })
   ],
   secret: process.env.NEXTAUTH_SECRET,
