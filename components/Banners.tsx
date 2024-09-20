8edc51fcd98420505ecece5a59e1f8db198ccdcf
Hai <limhaifc@gmail.com>
2024-09-20 14:45:16 +0700

    fix  banners

--- components/Banners.tsx
+++ components/Banners.tsx
@@ -1,17 +1,47 @@
+import Link from "next/link";
+
 const Banner = () => {
   return (
     <div className="bg-indigo-600">
       <div className="max-w-screen-xl mx-auto px-4 py-3 text-white sm:text-center md:px-8">
         <p className="font-medium">
-          យើងទើបតែបើកដំណើរការប្លក់ថ្មីនៅថ្ងៃនេះ <a href="/" className="font-semibold underline duration-150 hover:text-indigo-100 inline-flex items-center gap-x-1">
-          ស្វែងយល់បន្ថែម
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-            <path fillRule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clipRule="evenodd" />
-          </svg>
-        </a>
+          យើងទើបតែបើកដំណើរការប្លក់ថ្មីនៅថ្ងៃនេះ{" "}
+          <Link
+            href="/"
+            className="font-semibold underline duration-150 hover:text-indigo-100 inline-flex items-center gap-x-1"
+          >
+            ស្វែងយល់បន្ថែម
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-5 h-5"
+              fill="none"
+              viewBox="0 0 64 64"
+              role="presentation"
+              aria-hidden="true"
+              focusable="false"
+            >
+              <path
+                fill="#FFC017"
+                d="m39.637 40.831-5.771 15.871a1.99 1.99 0 0 1-3.732 0l-5.771-15.87a2.02 2.02 0 0 0-1.194-1.195L7.298 33.866a1.99 1.99 0 0 1 0-3.732l15.87-5.771a2.02 2.02 0 0 0 1.195-1.194l5.771-15.871a1.99 1.99 0 0 1 3.732 0l5.771 15.87a2.02 2.02 0 0 0 1.194 1.195l15.871 5.771a1.99 1.99 0 0 1 0 3.732l-15.87 5.771a2.02 2.02 0 0 0-1.195 1.194"
+              ></path>
+            </svg>
+          </Link>
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
+
 export default Banner;
